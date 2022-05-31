7db90fb89dfaa02adc5e0d93c74cd8fdc9852eb6
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-06-01 01:25:04 +0530

    Create merge2SortedList.java

---
+++ Linked List/merge2SortedList.java
@@ -0,0 +1,34 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+        ListNode p1 = list1;
+        ListNode p2 = list2;
+        
+        ListNode head = new ListNode();
+        ListNode curr =head;
+        while(p1!=null && p2!=null){
+            if(p1.val<p2.val){
+                curr.next = p1;
+                p1=p1.next;
+                curr= curr.next;
+            }
+            else {
+                curr.next =p2;
+                p2 =p2.next;
+                curr=curr.next;
+                
+            }
+        }
+        while(p1!=null){
+              curr.next = p1;
+               p1=p1.next;
+               curr= curr.next;
+        }
+        while(p2!=null){
+            curr.next =p2;
+             p2 =p2.next;
+             curr=curr.next;
+        }
+        return head.next;
+    }
+}
