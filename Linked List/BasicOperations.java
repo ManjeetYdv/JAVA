c9571544d73b72a1a36c141ed394115a13ab1560
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-05-31 21:03:04 +0530

    Update BasicOperations.java

--- Linked List/BasicOperations.java
+++ Linked List/BasicOperations.java
@@ -10,6 +10,19 @@ public class LL {
         this.size = 0;
     }
 
+    private class Node{
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
 
     public void InsertAtFirst(int value){                      //Insert At First
         Node node = new Node(value);
@@ -142,17 +155,5 @@ public class LL {
 
     }
 
-    private class Node{
-        private int value;
-        private Node next;
 
-        public Node(int value) {
-            this.value = value;
-        }
-
-        public Node(int value, Node next) {
-            this.value = value;
-            this.next = next;
-        }
-    }
 }
