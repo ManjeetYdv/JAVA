90c91fa8fc6d17e02ac1f9a7953385edfd633dd0
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-06-03 19:51:21 +0530

    Update Q_304.java

--- leetcode/Q_304.java
+++ leetcode/Q_304.java
@@ -30,4 +30,12 @@ class NumMatrix {
  * Your NumMatrix object will be instantiated and called as such:
  * NumMatrix obj = new NumMatrix(matrix);
  * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ 
+ in brute force you can get the resulst in O(n^2) for each sumregion calculations. We can avoid O(n^2) for each calculations by only perform on O(n^2) when we create a prefix array, O(n) for each sumregion calculation.
+
+1- Create a prefix array with one extra empty row at index 0 and one empty column at index0 so you can avoid edge cases.
+2- loop through each row (from row1 +1 to row2+1 since we have an extra row)
+3- get the sum of each row at given columns (column2 +1 - the prefix sum of the given column + 1 -1))
+
+this will avoid O(n^2) time complexity in calculating sumregion and gets the result in O(n);
  */
