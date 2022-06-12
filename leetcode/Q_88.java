18573069d89ff385ef849bfedac5ca472cc1dc82
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-06-12 23:29:05 +0530

    Update Q_88.java

--- leetcode/Q_88.java
+++ leetcode/Q_88.java
@@ -1,39 +1,26 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        
-        int i=0;
-        int j=0;
-        int k=0;
-        
-        int[] arr=  new int[m+n];
-        
-            while(i<m && j<n){
-            if(nums1[i]<nums2[j]){
-                arr[k++]=nums1[i];
-                i++;
-            }
-            else if(nums1[i]>nums2[j]){
-                arr[k++]=nums2[j];
-                j++;
-            }
-            else if(nums1[i]==nums2[j]){
-                arr[k++]=nums1[i];
-                arr[k++]=nums2[j];
-                i++;
-                j++;
-            }
-                
-         }
-        
-        while(i<m){
-            arr[k++] = nums1[i++];
+       
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+    while (i >= 0 && j >= 0)
+    {
+        if (nums1[i] > nums2[j])
+        {
+            nums1[k--] = nums1[i--];
         }
-        while(j<n){
-            arr[k++]=nums2[j++];
-        }
-        for(int p=0 ;p<m+n ;p++){
-            nums1[p]=arr[p];
+        else
+        {
+            nums1[k--] = nums2[j--];
         }
     }
-    
+
+    while (j >= 0)
+    {
+        nums1[k--] = nums2[j--];
+    }
+      }
 }
+              
