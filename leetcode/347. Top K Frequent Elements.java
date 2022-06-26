8aef7b32be0969db16141fe43a78a66922a8cce9
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-06-27 00:04:04 +0530

    Create 347. Top K Frequent Elements.java

---
+++ leetcode/347. Top K Frequent Elements.java
@@ -0,0 +1,31 @@
+class Solution {
+    
+    
+    public int[] topKFrequent(int[] nums, int k) {
+        
+        
+       if(nums.length == 0){
+        return new int[k];
+        }
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->(map.get(a)-map.get(b)));      // this heapify function will sort the queue from min to max and minimum value will be placed at the top
+        for(int n : map.keySet()){
+            pq.add(n);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        int i = 0;
+        int[] result = new int[k];
+        while(pq.size()!=0){
+            int rem = pq.poll();
+            result[i++] = rem;
+        }
+
+        return result;
+    }
+}
