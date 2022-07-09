2d7154a9ac19bcc9a9ec0c543958a6f5189699bf
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-07-09 23:22:27 +0530

    Update 1696. Jump Game VI.java

--- leetcode/1696. Jump Game VI.java
+++ leetcode/1696. Jump Game VI.java
@@ -1,4 +1,4 @@
-// TLE
+// TLE ,then optimized use heap
 
 class Solution {
     
@@ -31,6 +31,38 @@ class Solution {
         }
 
         return sum[sum.length-1];
+        
+        
+    }
+}
+
+//optimized
+
+class Solution {
+    
+   
+    
+    public int maxResult(int[] arr, int k) {
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a , b)->b[1]-a[1]);
+        
+        int max=arr[0];
+        pq.add(new int[]{0 ,max});
+        
+        for(int i=1 ;i<arr.length ;i++){
+            
+            while(i-pq.peek()[0]>k){
+                pq.poll();
+            }
+            
+            int[]top = pq.peek();
+            max =arr[i]+top[1];
+            
+            pq.add(new int[]{i ,max});
+            
+        }
+        
+        return max;
         
         
     }
