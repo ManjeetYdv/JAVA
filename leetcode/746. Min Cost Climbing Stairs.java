3829adcd3ef0194bde554e4162d09d49cd604605
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-07-10 23:27:56 +0530

    Update 746. Min Cost Climbing Stairs.java

--- leetcode/746. Min Cost Climbing Stairs.java
+++ leetcode/746. Min Cost Climbing Stairs.java
@@ -2,22 +2,34 @@ class Solution {
     public int minCostClimbingStairs(int[] cost) {
         
         
-        List<Integer> list = new ArrayList<>();
+//         List<Integer> list = new ArrayList<>();
          
-        list.add(0);
-        list.add(0);
+//         list.add(0);
+//         list.add(0);
     
-        int l;
-        for(int i=0 ;i<cost.length ;i++){
+//         int l;
+//         for(int i=0 ;i<cost.length ;i++){
             
-           l= list.size();
-           int min = cost[i]+ Integer.min(list.get(l-2) ,list.get(l-1));
-           list.add(min);
+//            l= list.size();
+//            int min = cost[i]+ Integer.min(list.get(l-2) ,list.get(l-1));
+//            list.add(min);
         
+//         }
+        
+//         l = list.size();
+//         return Integer.min( list.get(l-1) , list.get(l-2));
+        
+        int prev1=0;
+        int prev2=0;
+        
+        for(int i: cost){
+            
+            int temp=prev2;
+            prev2=prev1;
+            prev1 =i+Integer.min(temp ,prev1);
         }
         
-        l = list.size();
-        return Integer.min( list.get(l-1) , list.get(l-2));
+        return Integer.min(prev1, prev2);
         
         
         
