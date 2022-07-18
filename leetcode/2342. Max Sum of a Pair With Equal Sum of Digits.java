39ade4d7b7496508e2fa3ec4b93b4df1ea13078e
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-07-18 16:14:11 +0530

    Update 2342. Max Sum of a Pair With Equal Sum of Digits.java

--- leetcode/2342. Max Sum of a Pair With Equal Sum of Digits.java
+++ leetcode/2342. Max Sum of a Pair With Equal Sum of Digits.java
@@ -13,55 +13,27 @@ class Solution {
     
     
     
-      public int max2Sum(List<Integer> list){
-
-        int max=0;
-        int max2=0;
-        for(int i= 0; i<list.size() ;i++){
-
-            if(list.get(i)>=max){
-                max2 = max;
-                max = list.get(i);
-            }
-            else if(list.get(i)>max2 && list.get(i)!=max){
-                max2 = list.get(i);
-            }
-        }
-           return max+max2;
-      }
-    
     
     
     
     public int maximumSum(int[] arr) {
         
-        int max=0;
-         for(int i=0 ;i<arr.length ;i++ ){
-            max= Math.max(max , sumDigits(arr[i]));
-        }
-        
-        List<List<Integer>> res= new ArrayList<>();
-        for(int i=0 ;i<=max; i++){
-            List<Integer> list = new ArrayList<>();
-            res.add(list);
-        }
-        
-        for(int i :arr){
+       int[] map = new int[82];
+        int result = -1;
+
+        for (int item : arr) {
+            int key = sumDigits(item);
+
+            if (map[key]!= 0){
+                result = Math.max(result, map[key] + item);
+            }
             
-            int pos = sumDigits(i);
-            res.get(pos).add(i);
-        }
-        
-        int maxSum=-1;
-        for(int i=0 ;i<=max; i++){
+            map[key] = Math.max(map[key], item);
             
-            List<Integer> temp = res.get(i);
-            if(temp.size()<=1) continue;
-            
-            maxSum = Integer.max(maxSum , max2Sum(temp) );
         }
+
+        return result;
         
-        return maxSum;
         
     }
 }
