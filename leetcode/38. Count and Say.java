93e5d309de4a6d57470a04ee79a181c8b7d2d1f2
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-10-18 23:15:53 +0530

    Update 38. Count and Say.java

--- leetcode/38. Count and Say.java
+++ leetcode/38. Count and Say.java
@@ -1,29 +1,60 @@
+// class Solution {
+//     public String countAndSay(int n) {
+//         if (n == 1) return "1";
+
+//         String a ="1";
+        
+//         for (int i = 1; i < n; i++) {    
+//             String b= "";
+            
+//             for(int j=0 ;j<a.length() ;j++){  //
+//                 char c = a.charAt(j);
+//                 int count=0;
+//                 int k=j;
+                
+//                 for(k=j ;k<a.length();k++){
+//                     if(a.charAt(k)==c) count++;
+//                     else break;
+//                 }
+//                 b+=Integer.toString(count)+c;
+//                 j=k-1;
+//             }
+            
+//             a=b;
+//         }
+        
+//         return a;
+
+//     }
+// }
 class Solution {
     public String countAndSay(int n) {
-        if (n == 1) return "1";
-
-        String a ="1";
-        
-        for (int i = 1; i < n; i++) {    
-            String b= "";
-            
-            for(int j=0 ;j<a.length() ;j++){  //
-                char c = a.charAt(j);
-                int count=0;
-                int k=j;
-                
-                for(k=j ;k<a.length();k++){
-                    if(a.charAt(k)==c) count++;
-                    else break;
-                }
-                b+=Integer.toString(count)+c;
-                j=k-1;
-            }
-            
-            a=b;
+        if (n ==1) {
+          return "1";
         }
-        
-        return a;
 
+        StringBuilder sb = new StringBuilder("1");
+        for (int i = 2; i <= n; i++) {
+            sb = getNextState(sb);
+        }
+        return sb.toString();
+    }
+
+    private StringBuilder getNextState(StringBuilder curSb) {
+        StringBuilder nextSb = new StringBuilder();
+        int len = curSb.length();
+        int i = 0;
+		
+        while (i < len) {
+            char c = curSb.charAt(i++);
+            int count = 1;
+            while (i < len && c == curSb.charAt(i)) {
+                count++;
+                i++;
+            }
+            nextSb.append(count).append(c);
+        }
+		
+        return nextSb;
     }
 }
