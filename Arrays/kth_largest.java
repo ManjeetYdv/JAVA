a454e8a3c05ea89dc4fab62071b04199454019ba
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-10-21 14:53:02 +0530

    Update kth_largest.java

--- Arrays/kth_largest.java
+++ Arrays/kth_largest.java
@@ -1,30 +1,17 @@
-package com.company;
-
-import java.util.PriorityQueue;
-
-public class kth_largest {
-
-    public static void main(String[] args) {
-        int[] arr ={40,  30 , 20 , 10};
-        int k=2;
-        int kth =kthlargest(arr , k);
-        System.out.println("kth largest element : "+ kth);
-    }
-
-    public static int kthlargest(int[] arr , int k){
-
-            PriorityQueue<Integer> pq = new PriorityQueue<>();   //For kth largest , we need a min-heap
-
-        for(int i=0;i<k ;i++){
-            pq.add(arr[i]);
-        }
-
-        for(int i=k; i<arr.length ;i++) {
-            if (pq.peek() < arr[i]) {
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        
+         PriorityQueue<Integer> pq = new PriorityQueue<>();
+         
+        for(int i=0 ;i<nums.length ;i++){
+            pq.add(nums[i]);
+            
+            if(pq.size()>k){
                 pq.poll();
-                pq.add(arr[i]);
             }
         }
-       return pq.peek();
+        
+        return pq.peek();
+        
     }
 }
