12a1cbb20aeff43285759917a6887e6387a12737
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-11-12 02:49:49 +0530

    Update 27. Remove Element.java

--- leetcode/27. Remove Element.java
+++ leetcode/27. Remove Element.java
@@ -1,20 +1,9 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        
-         if(nums.length == 0)
-            return 0;
-         
-         int k = 0;
-         for(int i = 0 ; i < nums.length; i++) {
-              
-            if(nums[i] == val) continue;
-              
-            nums[k] = nums[i];
-            k++;
+        int idx=0;
+        for(int i=0 ;i<nums.length ;i++){
+            if(nums[i]!=val) nums[idx++] = nums[i];
         }
-        return k;
-       }
+        return idx; 
     }
-
-        
-    
+}
