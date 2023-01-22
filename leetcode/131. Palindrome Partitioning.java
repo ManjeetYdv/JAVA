07396aafa1071cea8b2a60e0fdae592193f23e13
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2023-01-23 03:29:03 +0530

    Update 131. Palindrome Partitioning.java

--- leetcode/131. Palindrome Partitioning.java
+++ leetcode/131. Palindrome Partitioning.java
@@ -1,52 +1,32 @@
 class Solution {
-    
     List<List<String>> res;
-    
-    
-    
-    public void func(String s , int index , List<String> list){
-        
+    public void helper(int index , String s , List<String> list){
+
         if(index==s.length()){
             res.add(new ArrayList<>(list));
             return;
         }
-        
-        for(int i =index ;i<s.length() ;i++){
-            
-            if(isPalindrome(s , index , i)){
-                
-                list.add(s.substring(index ,i+1));
-                func(s , i+1 ,list);
+
+        for(int i=index ; i<s.length() ;i++){
+            if(palindromeCheck(s.substring(index  , i+1))){
+                list.add(s.substring(index , i+1));
+                helper(i+1 , s , list);
                 list.remove(list.size()-1);
-            
             }
         }
-        
     }
-   
-   
-    public List<List<String>> partition(String s) {
-        
-        res = new ArrayList<>();
-        func(s,  0,  new ArrayList<>());
-        return res;
-        
-        
-    }
-    
-    
-      public boolean isPalindrome(String str , int start , int end){
-        
-        while(start<=end){
-            
-            if(str.charAt(start)==str.charAt(end)){
-                start++;
-                end--;
-            }
-            else{
-                return false;
-            }
+
+    public boolean palindromeCheck(String str){
+        int start=0;
+        int end = str.length()-1;
+        while(start<end) {
+            if(str.charAt(start++)!=str.charAt(end--)) return false;
         }
         return true;
+    }
+    public List<List<String>> partition(String s) {
+        res = new ArrayList<>();
+        helper(0 ,s , new ArrayList<>());
+        return res;
     }
 }
